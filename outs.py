c5bbcfd5cd2a11cef3b259df311f167a47dcc6cc
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-08 13:13:48 -0700

    Fix these, which broke with recent change in gl API

--- outs.py
+++ outs.py
@@ -7,14 +7,12 @@ teams = {}
 team_template = {G:0, O:0}
 
 for gm in gl.gamelogs(2015):
-    for tm in gm['teams']:
-        team = gl.getteam(tm['Name'], teams, team_template)
+    for tm in gm.teams:
+        team = gl.getteam(tm.Name, teams, team_template)
         team[G] += 1
-        team[O] += gm[O]
+        team[O] += gm.details[O]
 
 for teamname in teams:
     tm = teams[teamname]
     outputs = (teamname, tm[O], tm[G], tm[O]-54*tm[G])
     print ('|'.join(map(str, outputs)))
-
-
