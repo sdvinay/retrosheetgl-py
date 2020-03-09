ba5a573b7023d0494806f1bbf5e4b94d05f3c813
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-08 21:29:54 -0700

    Generalize teammates.py to any number of teammates

--- teammates.py
+++ teammates.py
@@ -2,16 +2,24 @@ import gl
 import glutils
 import itertools
 
-teammate_games = {}
-for gm in gl.gamelogs(2018, 2019):
+# Number of games started together by sets of teammates
+
+NUM_STARTERS = 9
+
+teammate_numgames = {}
+teammate_firstgame = {}
+for gm in gl.gamelogs(1969, 2019):
     for tm in gm.teams.values():
-        lineup = tm.lineup
-        for combo in itertools.combinations(lineup, 2):
-            pair = (combo[0]['ID'], combo[1]['ID'])
-            if pair not in teammate_games:
-                teammate_games[pair] = 0
-            teammate_games[pair] += 1
+        lineup = [player['ID'] for player in tm.lineup]
+        for teammates in itertools.combinations(sorted(lineup), NUM_STARTERS):
+            if teammates not in teammate_numgames:
+                teammate_numgames[teammates] = 0
+                teammate_firstgame[teammates] = gm.details['DateStr']
+            teammate_numgames[teammates] += 1
 
 
-for pair in teammate_games:
-    print(teammate_games[pair], tuple(map(glutils.getplayername, pair)))
+sorted_d = sorted((value, key) for (key, value) in teammate_firstgame.items())
+for (dt, teammates) in sorted_d:
+    if teammate_numgames[teammates] > 50:
+        playernames = tuple(map(glutils.getplayername, teammates))
+        print(teammate_numgames[teammates], dt, playernames)
