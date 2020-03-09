5377ef5bd291099035fb36a698ed5bc453b54685
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-08 21:39:02 -0700

    Use an Enum for HOME/AWAY rather than int or IntEnum

--- nohitters.py
+++ nohitters.py
@@ -4,12 +4,12 @@ import glutils
 
 for gm in gl.gamelogs(2017, 2018):
     tms = gm.teams
-    for tm in tms:
+    for tm in tms.values():
         if tm.opp.stats['bat']['H'] == 0:
             for player in tm.lineup:
                 playerId = player['ID']
                 playerName = glutils.getplayername(playerId)
                 print(playerId, playerName)
 
-            for tm in tms:
+            for tm in tms.values():
                 print(tm.linescore, tm.stats['bat'])
