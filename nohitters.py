c7e5d3c0b83ddddacdd9386e1e2a5c28a753a25a
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-08 17:50:10 -0700

    Move convenience functions to glutils

--- nohitters.py
+++ nohitters.py
@@ -1,4 +1,5 @@
 import gl
+import glutils
 
 
 for gm in gl.gamelogs(2017, 2018):
@@ -7,7 +8,7 @@ for gm in gl.gamelogs(2017, 2018):
         if tm.opp.stats['bat']['H'] == 0:
             for player in tm.lineup:
                 playerId = player['ID']
-                playerName = gl.getplayername(playerId)
+                playerName = glutils.getplayername(playerId)
                 print(playerId, playerName)
 
             for tm in tms:
