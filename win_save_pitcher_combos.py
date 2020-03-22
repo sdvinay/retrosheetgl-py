547fcf377eae27cab029dac2e11e5c565f679baf
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 16:38:21 -0700

    win_save_pitcher_combos: readability

--- win_save_pitcher_combos.py
+++ win_save_pitcher_combos.py
@@ -1,20 +1,20 @@
 import retrosheetgl as gl
 import glutils
 
-# count and display winners/saver combinations
+# count and display winners/saver pairs
 
 
-# map of playerId pairs to array of dates on when they achieved those combos
-combos = {}
+# map of playerId pairs to array of dates on when they achieved those
+winsave_pair_games = {}
 
 for gm in gl.gamelogs(2017, 2019):
     recs = gm.record
     if recs.get('save_pitcher'):
-        combo = (recs['save_pitcher'], recs['winning_pitcher'])
-        combo_dates = glutils.getentity(combo, combos, [])
-        combo_dates.append(gm.details['DateStr'])
+        winsave_pair = (recs['save_pitcher'], recs['winning_pitcher'])
+        pair_dates = glutils.getentity(winsave_pair, winsave_pair_games, [])
+        pair_dates.append(gm.details['DateStr'])
 
-for combo in combos:
-    if len(combos[combo]) > 10:
-        player_names = map(glutils.getplayername, combo[0:2])
-        print(len(combos[combo]), tuple(player_names))
+for winsave_pair in winsave_pair_games:
+    if len(winsave_pair_games[winsave_pair]) > 10:
+        player_names = map(glutils.getplayername, winsave_pair[0:2])
+        print(len(winsave_pair_games[winsave_pair]), tuple(player_names))
