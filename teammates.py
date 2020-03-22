2f43cbcbc48805ac03c8a42b48e80902867f5af2
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 15:24:42 -0700

    teammates: improve readability

--- teammates.py
+++ teammates.py
@@ -4,20 +4,20 @@ import itertools
 
 # Number of games started together by sets of teammates
 
-NUM_STARTERS = 2
+NUM_COMMON_STARTERS = 2
 
 
-def yield_teammate_combos(gm, NUM_STARTERS_TOGETHER):
+def yield_teammate_combos(gm, numTeammates):
     for tm in gm.teams.values():
         lineup = [player['ID'] for player in tm.lineup]
-        for teammates in itertools.combinations(sorted(lineup), NUM_STARTERS_TOGETHER):
+        for teammates in itertools.combinations(sorted(lineup), numTeammates):
             yield teammates
 
 
 teammate_numgames = {}
 teammate_firstgame = {}
-for gm in gl.gamelogs(1969, 2019):
-    for teammates in yield_teammate_combos(gm, NUM_STARTERS):
+for gm in gl.gamelogs(2000, 2019):
+    for teammates in yield_teammate_combos(gm, NUM_COMMON_STARTERS):
         if teammates not in teammate_numgames:
             teammate_numgames[teammates] = 0
             teammate_firstgame[teammates] = gm.details['DateStr']
