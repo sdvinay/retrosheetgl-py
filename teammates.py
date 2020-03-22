6faeadf998b7258fa0dfd9006afe3fd23de403f8
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 12:39:39 -0700

    teammates: pulled yield_teammate_combos() into its own function

--- teammates.py
+++ teammates.py
@@ -4,22 +4,28 @@ import itertools
 
 # Number of games started together by sets of teammates
 
-NUM_STARTERS = 9
+NUM_STARTERS = 2
+
+
+def yield_teammate_combos(gm, NUM_STARTERS_TOGETHER):
+    for tm in gm.teams.values():
+        lineup = [player['ID'] for player in tm.lineup]
+        for teammates in itertools.combinations(sorted(lineup), NUM_STARTERS_TOGETHER):
+            yield teammates
+
 
 teammate_numgames = {}
 teammate_firstgame = {}
 for gm in gl.gamelogs(1969, 2019):
-    for tm in gm.teams.values():
-        lineup = [player['ID'] for player in tm.lineup]
-        for teammates in itertools.combinations(sorted(lineup), NUM_STARTERS):
-            if teammates not in teammate_numgames:
-                teammate_numgames[teammates] = 0
-                teammate_firstgame[teammates] = gm.details['DateStr']
-            teammate_numgames[teammates] += 1
+    for teammates in yield_teammate_combos(gm, NUM_STARTERS):
+        if teammates not in teammate_numgames:
+            teammate_numgames[teammates] = 0
+            teammate_firstgame[teammates] = gm.details['DateStr']
+        teammate_numgames[teammates] += 1
 
 
 sorted_d = sorted((value, key) for (key, value) in teammate_firstgame.items())
 for (dt, teammates) in sorted_d:
-    if teammate_numgames[teammates] > 50:
+    if teammate_numgames[teammates] > 500:
         playernames = tuple(map(glutils.getplayername, teammates))
         print(teammate_numgames[teammates], dt, playernames)
