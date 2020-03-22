5e799bd392e87f37d21a32d435842209ceb14305
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 14:21:09 -0700

    nohitters: count no-hitters started by player

--- nohitters.py
+++ nohitters.py
@@ -2,14 +2,19 @@ import gl
 import glutils
 
 
-for gm in gl.gamelogs(2017, 2018):
-    tms = gm.teams
-    for tm in tms.values():
-        if tm.opp.stats['bat']['H'] == 0:
-            for player in tm.lineup:
-                playerId = player['ID']
-                playerName = glutils.getplayername(playerId)
-                print(playerId, playerName)
+# Count the no-hitters that each player has started in
 
-            for tm in tms.values():
-                print(tm.linescore, tm.stats['bat'])
+# map of players to the dates of nohitters they've started in
+nohitter_starts = {}
+
+for gm in gl.gamelogs(2000, 2019):
+    for tm in gm.teams.values():
+        if tm.opp.stats['bat'] and tm.opp.stats['bat']['H'] == 0:
+            for player in tm.lineup:
+                starts = glutils.getentity(player['ID'], nohitter_starts, [])
+                starts.append(gm.details['DateStr'])
+
+# print out the players who've started the most no-hitters
+for player in nohitter_starts:
+    if len(nohitter_starts[player]) > 3:
+        print(glutils.getplayername(player), nohitter_starts[player])
