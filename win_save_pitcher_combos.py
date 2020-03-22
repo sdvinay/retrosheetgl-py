893634a88e734a49ab641d77e46a178f5e4782db
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 16:33:08 -0700

    win_save_pitcher_combos: track dates of combos, not just the count of them

--- win_save_pitcher_combos.py
+++ win_save_pitcher_combos.py
@@ -1,17 +1,20 @@
 import retrosheetgl as gl
 import glutils
 
+# count and display winners/saver combinations
 
+
+# map of playerId pairs to array of dates on when they achieved those combos
 combos = {}
+
 for gm in gl.gamelogs(2017, 2019):
     recs = gm.record
     if recs.get('save_pitcher'):
         combo = (recs['save_pitcher'], recs['winning_pitcher'])
-        if combo not in combos:
-            combos[combo] = 0
-        combos[combo] += 1
+        combo_dates = glutils.getentity(combo, combos, [])
+        combo_dates.append(gm.details['DateStr'])
 
 for combo in combos:
-    if combos[combo] > 10:
+    if len(combos[combo]) > 10:
         player_names = map(glutils.getplayername, combo[0:2])
-        print(combos[combo], tuple(player_names))
+        print(len(combos[combo]), tuple(player_names))
