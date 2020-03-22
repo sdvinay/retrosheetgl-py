ae039ad37b3ce4f92e017602effd1a119f0b569f
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-22 16:25:25 -0700

    win_save_pitcher_combos.py: span multiple years, rather than single season

--- win_save_pitcher_combos.py
+++ win_save_pitcher_combos.py
@@ -3,15 +3,15 @@ import glutils
 
 
 combos = {}
-for gm in gl.gamelogs(2019):
+for gm in gl.gamelogs(2017, 2019):
     recs = gm.record
     if recs.get('save_pitcher'):
-        combo = (recs['save_pitcher'], recs['winning_pitcher'], gm.year)
+        combo = (recs['save_pitcher'], recs['winning_pitcher'])
         if combo not in combos:
             combos[combo] = 0
         combos[combo] += 1
 
 for combo in combos:
-    if combos[combo] > 3:
+    if combos[combo] > 10:
         player_names = map(glutils.getplayername, combo[0:2])
         print(combos[combo], tuple(player_names))
