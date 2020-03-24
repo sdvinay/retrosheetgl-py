1fd456bb028307719a93fa234a5aa6b4bb8a6dae
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-23 21:19:57 -0700

    Added tests for gl.parse_linescore_str

--- test_gl.py
+++ test_gl.py
@@ -1,2 +1,14 @@
-def test_hello_world():
-    assert 2+2 == 4
+import retrosheetgl as gl
+
+
+def test_linescore():
+    parser = gl.parse_linescore_str
+    assert parser('0') == [0]
+    assert parser('00') == [0, 0]
+    assert parser('x') == [None]
+    assert parser('1') == [1]
+    assert parser('(11)') == [11]
+    assert parser('0(11)0') == [0, 11, 0]
+    assert parser('0(11)0(20)') == [0, 11, 0, 20]
+    assert parser('0(11)(20)0') == [0, 11, 20, 0]
+    assert parser('001002030') == [0, 0, 1, 0, 0, 2, 0, 3, 0]
