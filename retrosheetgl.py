47fc83a8282d5074c2290c2631359e091b96e1f7
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-03-24 09:10:52 -0700

    Add SPs' player names to lookup table

--- retrosheetgl.py
+++ retrosheetgl.py
@@ -173,7 +173,9 @@ def parse_game_line(gmline):
         tm.stats['game'] = {G: tm.G, W: tm.W, L: tm.L, RS: tm.RS, RA: tm.RA}
         tm.lineup = parse_lineup(gmline, homeOrAway)
         tm.linescore = parse_linescore(gmline, homeOrAway)
-        tm.starter = gmline[{HA.home: 104, HA.away: 102}[homeOrAway]-1]
+        SP_fieldnum = {HA.home: 104, HA.away: 102}[homeOrAway]
+        tm.starter = gmline[SP_fieldnum-1]
+        glutils.addplayername(tm.starter, gmline[SP_fieldnum])
 
     gm.record = parse_players_of_record(gmline)
 
