fe247800be1d498d3e54f563f0ce1014a07929f1
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-04-30 18:24:35 -0700

    Fixed test_teammates by making teammates importable

--- teammates.py
+++ teammates.py
@@ -13,20 +13,20 @@ def yield_teammate_combos(tm, numTeammates):
         yield teammates
 
 
-teammate_numgames = {}
-teammate_firstgame = {}
-for gm in gl.gamelogs(1969, 2019, "./glfiles/"):
-    for tm in gm.teams.values():
-        if tm.Name == 'SDN':
-            for teammates in yield_teammate_combos(tm, NUM_COMMON_STARTERS):
-                if teammates not in teammate_numgames:
-                    teammate_numgames[teammates] = 0
-                    teammate_firstgame[teammates] = gm.details['DateStr']
-                teammate_numgames[teammates] += 1
+if __name__ == "__main__":
+    teammate_numgames = {}
+    teammate_firstgame = {}
+    for gm in gl.gamelogs(1969, 2019, "./glfiles/"):
+        for tm in gm.teams.values():
+            if tm.Name == 'SDN':
+                for teammates in yield_teammate_combos(tm, NUM_COMMON_STARTERS):
+                    if teammates not in teammate_numgames:
+                        teammate_numgames[teammates] = 0
+                        teammate_firstgame[teammates] = gm.details['DateStr']
+                    teammate_numgames[teammates] += 1
 
-
-sorted_d = sorted((value, key) for (key, value) in teammate_firstgame.items())
-for (dt, teammates) in sorted_d:
-    if teammate_numgames[teammates] > 200:
-        playernames = tuple(map(glutils.getplayername, teammates))
-        print(teammate_numgames[teammates], dt, playernames)
+    sorted_d = sorted((v, k) for (k, v) in teammate_firstgame.items())
+    for (dt, teammates) in sorted_d:
+        if teammate_numgames[teammates] > 200:
+            playernames = tuple(map(glutils.getplayername, teammates))
+            print(teammate_numgames[teammates], dt, playernames)
